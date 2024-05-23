7864dfa552e376d2f4ff9dc709c05e9c922b3343
CaiXuefeng <1023829962@qq.com>
2024-05-23 14:47:55 +0800

    upload usb code

--- 2_examples/2_Arduino/basic_routines/03_exti/exti.h
+++ 2_examples/2_Arduino/basic_routines/03_exti/exti.h
@@ -1,23 +1,19 @@
 /**
  ****************************************************************************************************
  * @file        exti.h
- * @author      正点原子团队(ALIENTEK)
- * @version     V1.0
- * @date        2023-12-01
- * @brief       EXTI 驱动代码
- * @license     Copyright (c) 2020-2032, 广州市星翼电子科技有限公司
+ * @author      ALIENTEK
+ * @brief       EXTI code
+ * @license     Copyright (C) 2020-2030, ALIENTEK
  ****************************************************************************************************
  * @attention
  *
- * 实验平台:正点原子 ESP32S3 开发板
- * 在线视频:www.yuanzige.com
- * 技术论坛:www.openedv.com
- * 公司网址:www.alientek.com
- * 购买地址:openedv.taobao.com
+ * platform     : ALIENTEK DNESP32S3 board
+ * website      : www.alientek.com
+ * forum        : www.openedv.com/forum.php
  *
- * 修改说明
- * V1.0 20231201
- * 第一次发布
+ * change logs  :
+ * version      data         notes
+ * V1.0         20240430     the first version
  *
  ****************************************************************************************************
  */
